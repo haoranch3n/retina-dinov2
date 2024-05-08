16d79010880eea248ca32dc0c13c055e35eed63d
haoranch3n <hchen19@stjude.org>
2024-05-08 17:01:01 -0500

    train

--- dinov2/data/datasets/retina_fundus.py
+++ dinov2/data/datasets/retina_fundus.py
@@ -1,106 +1,67 @@
-import csv
-from enum import Enum
-import logging
+# This file is a modified version of the original file from the DINO repository.
+from typing import Any, Optional, Callable, Tuple
+
+from PIL import Image
+
 import os
-from typing import Callable, List, Optional, Tuple, Union
 
-import numpy as np
+from dinov2.data.datasets.extended import ExtendedVisionDataset
 
-from .extended import ExtendedVisionDataset
-
-logger = logging.getLogger("dinov2")
-_Target = int
-
-class _Split(Enum):
-    TRAIN = "train"
-    VAL = "val"
-    TEST = "test"  # NOTE: torchvision does not support the test split
-
-    @property
-    def length(self) -> int:
-        split_lengths = {
-            _Split.TRAIN: 1_281_167,
-            _Split.VAL: 50_000,
-            _Split.TEST: 100_000,
-        }
-        return split_lengths[self]
-
-    def get_dirname(self, class_id: Optional[str] = None) -> str:
-        return self.value if class_id is None else os.path.join(self.value, class_id)
-
-    def get_image_relpath(self, actual_index: int, class_id: Optional[str] = None) -> str:
-        dirname = self.get_dirname(class_id)
-        if self == _Split.TRAIN:
-            basename = f"{class_id}_{actual_index}"
-        else:  # self in (_Split.VAL, _Split.TEST):
-            basename = f"ILSVRC2012_{self.value}_{actual_index:08d}"
-        return os.path.join(dirname, basename + ".JPEG")
-
-    def parse_image_relpath(self, image_relpath: str) -> Tuple[str, int]:
-        assert self != _Split.TEST
-        dirname, filename = os.path.split(image_relpath)
-        class_id = os.path.split(dirname)[-1]
-        basename, _ = os.path.splitext(filename)
-        actual_index = int(basename.split("_")[-1])
-        return class_id, actual_index
+def get_image_files(dataset_path):
+    """
+    This function returns a list of all files in a directory and its subdirectories.
+    
+    :param dir: The directory path where you want to list all the files
+    :return: The function `list_files` returns a list of file paths for all the files in the directory
+    and its subdirectories.
+    """
+    images = []
+    for root, _, files in os.walk(dataset_path):
+        for name in files:
+            images.append(os.path.join(root, name))
+    return images
 
 class Fundus(ExtendedVisionDataset):
-    Target = Union[_Target]
-    Split = Union[_Split]
+    def __init__(self,
+                 root: str,
+                 transforms: Optional[Callable] = None,
+                    transform: Optional[Callable] = None,
+                    target_transform: Optional[Callable] = None) -> None:
 
-    def __init__(
-        self,
-        *,
-        split: "Fundus.Split",
-        root: str,
-        transforms: Optional[Callable] = None,
-        transform: Optional[Callable] = None,
-        target_transform: Optional[Callable] = None,
-    ) -> None:
         super().__init__(root, transforms, transform, target_transform)
-        self._split = split
-        print(split)
-        self._entries = None
-        self._class_ids = None
-        self._class_names = None
 
-    @property
-    def split(self) -> "Fundus.Split":
-        return self._split
+        self.root = root
+        self.image_paths = get_image_files(self.root)
+        self.transform =  transforms.Compose([
+                            transforms.Resize(252),
+                            transforms.CenterCrop(252),
+                            transforms.ToTensor(),
+                            transforms.Normalize([0.485, 0.456, 0.406],
+                                                [0.229, 0.224, 0.225])
+                        ])
 
-    def __len__(self) -> int:
-        return self._split.length
+    def get_image_data(self, index: int) -> bytes:  # should return an image as an array
 
-    def find_class_id(self, class_index: int) -> str:
-        class_ids = self._class_ids
-        return str(class_ids[class_index])
+        image_path = self.image_paths[index]
+        img = Image.open(image_path).convert(mode="RGB")
 
-    def find_class_name(self, class_index: int) -> str:
-        class_names = self._class_names
-        return str(class_names[class_index])
+        return img
 
-    def get_image_data(self, index: int) -> bytes:
-        actual_index = index + 1  # or however you determine the actual index
-        class_id = self.find_class_id(index)
+    def get_target(self, index: int) -> Any:
+        return 0
 
-        image_relpath = self.split.get_image_relpath(actual_index, class_id)
-        image_full_path = os.path.join(self.root, image_relpath)
-        with open(image_full_path, mode="rb") as f:
-            image_data = f.read()
-        return image_data
+    def __getitem__(self, index: int) -> Tuple[Any, Any]:
+        try:
+            image = self.get_image_data(index)
+        except Exception as e:
+            raise RuntimeError(f"can not read image for sample {index}") from e
+        # target = self.get_target(index)
 
-    def get_target(self, index: int) -> Optional[Target]:
-        if self.split == _Split.TEST:
-            return None
-        class_index = index  # or however you determine the class index
-        return int(class_index)
+        if self.transform is not None:
+            image = self.transform(image)
 
-    def get_class_id(self, index: int) -> Optional[str]:
-        if self.split == _Split.TEST:
-            return None
-        return str(index)  # or however you determine the class ID
+        return image
 
-    def get_class_name(self, index: int) -> Optional[str]:
-        if self.split == _Split.TEST:
-            return None
-        return "Class Name"  # or however you determine the class name
+    def __len__(self):
+        """Returns the total number of samples."""
+        return len(self.image_paths)
