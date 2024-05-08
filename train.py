3da8f7237381fc32a376b79d3fa0202d490eb97f
haoranch3n <hchen19@stjude.org>
2024-05-08 17:39:11 -0500

    image readin

--- train.py
+++ train.py
@@ -255,7 +255,10 @@ def do_train(cfg, model, resume=False):
         transform=data_transform,
         target_transform=lambda _: (),
     )
+    print('-----------')
     print(dataset)
+    print('-----------')
+
     # sampler_type = SamplerType.INFINITE
     sampler_type = SamplerType.SHARDED_INFINITE
     data_loader = make_data_loader(
@@ -269,7 +272,11 @@ def do_train(cfg, model, resume=False):
         drop_last=True,
         collate_fn=collate_fn,
     )
+    print('-----------')
+
     print(data_loader)
+    print('-----------')
+
     # training loop
 
     iteration = start_iter
