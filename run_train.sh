4a1101291eb20c806a27a8a5a74210bc8189c930
hchen19 <hchen19@stjude.org>
2024-05-09 01:16:57 +0000

    run train

--- run_train.sh
+++ run_train.sh
@@ -1,1 +1,3 @@
-python train.py --nodes 1 --config-file dinov2/configs/train/vitl16_short.yaml --output-dir /data train.dataset_path=ImageNet:split=TRAIN:root=/data/fundus
+export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
+python -m torch.distributed.launch --nproc_per_node=8 train_individual_worker.py --config-file dinov2/configs/train/vitb16_short.yaml --output-dir /cnvrg/result train.dataset_path=Fundus:root=/data/fundus
+
